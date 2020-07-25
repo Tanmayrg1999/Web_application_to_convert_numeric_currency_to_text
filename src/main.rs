89af68303c994e0b8e6b4ff12ab902db8373d3c6
Tanmay Ravindra Gaurkar <53641559+Tanmayrg1999@users.noreply.github.com>
2020-07-25 16:03:28 +0530

    Add files via upload
    Now the return type of Sink1() function was changed from string to template.
    Thus now the output was form as well as the corresponding output.this is known as template rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 #![allow(non_snake_case)]
-#![allow(unused)] 
+#![allow(unused)]
 #![feature(proc_macro_hygiene, decl_macro)]
 #[warn(unused_parens)]
 #[macro_use]
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
 use std::ffi::CString;
 #[macro_use] extern crate rocket;
 use rocket::request::{Form, FormError, FormDataError};
 use rocket::response::NamedFile;
 use rocket::http::RawStr;
 use std::str;
-extern crate reqwest;
 mod english;
 mod bengali;
 mod gujrati;
 mod marathi;
 mod hindi;
+use std::fs::File;
+use std::io::prelude::*;
+use rocket::response::Redirect;
+use rocket_contrib::templates::Template;
 #[cfg(test)] mod tests;
 #[derive(Debug, FromFormValue)]
 enum FormOption {
@@ -25,15 +30,22 @@ struct FormInput<>{
     number: f64,
     Language: FormOption,
 }
-
+#[derive(Serialize)]
+struct TemplateContext {
+    name: String,
+    items: String
+}
 #[post("/",rank=1, data = "<sink>")]
-fn sink1(sink:Form<FormInput>)  ->String{
+fn sink1(sink:Form<FormInput>)->Template{
 let mut output = String::new();
 let	x=sink.number;
 let intamount= x as i32;
 let _diff: f64 = x - f64::from(intamount);
 let _fn: f64 = _diff * f64::from(100);
 let _int_fn = _fn.round();
+output.push_str("Your currency value is :");
+output.push_str(&(sink.number).to_string());
+output.push_str("\n Text output is :");
 match sink.Language
 {
     FormOption::English=>{
@@ -49,7 +61,7 @@ if intamount>100&&intamount<1000000000 {
             output.push_str(&(_int_fn).to_string());
             output.push_str("/100");
         }
-    else
+    if(intamount<1&&intamount>100000000)
     {
         output.push_str("Enter a valid input.\nNo must be between 0.00-100000000.00");
     }
@@ -61,7 +73,8 @@ FormOption::Bengali=>{
             output.push_str(&(_int_fn).to_string());
             output.push_str("/100");
     }
-    else {
+        if(intamount<1&&intamount>100000000)
+ {
                 output.push_str("Enter a valid input.\nNo must be between 0.00-100000000.00");
     }
 }
@@ -71,7 +84,8 @@ FormOption::Gujrati=>{
             output.push_str(&*gujrati::hashmap_gujrati2(intamount));
             output.push_str(&(_int_fn).to_string());
             output.push_str("/100");}
-    else {
+        if(intamount<1&&intamount>100000000)
+ {
                         output.push_str("Enter a valid input.\nNo must be between 0.00-100000000.00");
     }
 }
@@ -82,7 +96,8 @@ FormOption::Hindi=>{
             output.push_str(&(_int_fn).to_string());
             output.push_str("/100");
      }
-     else {
+         if(intamount<1&&intamount>100000000)
+ {
             output.push_str("Enter a valid input.\nNo must be between 0.00-100000000.00");
     }
 }
@@ -93,20 +108,23 @@ FormOption::Marathi=>{
             output.push_str(&(_int_fn).to_string());
             output.push_str("/100");
 }
- else {
+     if(intamount<1&&intamount>100000000)
+ {
             output.push_str("Enter a valid input.\nNo must be between 0.00-100000000.00");
     }
 }
 }
-return output
-//format!("Your currency value is : {}\n{}",sink.number,output)
+let mut name1=String::new();
+name1="Your output is:".to_string();
+ let context = TemplateContext { name:name1, items: output };
+ Template::render("index", &context)
 }
 #[get("/",rank=2)]
 fn index() -> Option<NamedFile> {
     NamedFile::open("static/index.html").ok()
 }
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, sink1])
+    rocket::ignite().mount("/", routes![sink1,index]).attach(Template::fairing())
 }
 fn main() {
     rocket().launch();
